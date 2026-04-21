08d7f2832481189aa61f2bfa44bcf6dc44f52a21
Payal Sharma <payal3336plk@gmail.com>
2026-04-21 12:33:35 +0530

    finished

--- src/components/footer.py
+++ src/components/footer.py
@@ -4,8 +4,17 @@ import streamlit as st
 def footer_home():
 
     st.markdown(f"""
-    <div style = "display:flex; gap:6px; justify-content:center;margin-top:2rem">
+    <div style = "display:flex; gap:6px; justify-content:center;margin-top:2rem; items-align: center">
        <p style="font-weight:bold; color:white;"> Created with ♥️</p>
+    </div>
+        """, unsafe_allow_html = True
+    )
+
+def footer_dashboard():
+
+    st.markdown(f"""
+    <div style = "display: flex; gap: 6px; justify-content: center; margin-top:2rem; items-align: center">
+       <p style="font-weight:bold; color:black;"> Created with ♥️</p>
     </div>
         """, unsafe_allow_html = True
     )
--- src/ui/base_layout.py
+++ src/ui/base_layout.py
@@ -61,7 +61,6 @@ def style_base_layout():
                    font-size: 2rem !important;
                    line-height:0.9 !important;
                    margin-bottom: 0rem !important;
-                   color: black !important;
                 }
 
                 h3, h4, p{
@@ -70,7 +69,7 @@ def style_base_layout():
 
                 button{
                    border-radius: 1.5rem !important;
-                   background: #5865F2 !important;
+                   background-color: #5865F2 !important;
                    color: white !important;
                    padding: 10px 20px !important;
                    border: none !important;
@@ -79,7 +78,7 @@ def style_base_layout():
 
                 button[kind="secondary"]{
                    border-radius: 1.5rem !important;
-                   background: #EB459E !important;
+                   background-color: #EB459E !important;
                    color: white !important;
                    padding: 10px 20px !important;
                    border: none !important;
@@ -88,7 +87,7 @@ def style_base_layout():
 
                 button[kind="tertiary"]{
                    border-radius: 1.5rem !important;
-                   background: black !important;
+                   background-color: black !important;
                    color: white !important;
                    padding: 10px 20px !important;
                    border: none !important;
